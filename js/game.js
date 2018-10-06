76dbaff0602ce64ab7454de90d23fa919515e323
Lukas  A <lukasa707@gmail.com>
2018-10-06 19:12:07 +0200

    Assets y game.js actualizados
    Background.png se ha modificado para ajustarse a todas las pantallas, y game.js ha sido ampliado con controles para los jugadores y colisiones con el mundo y demás sprites.

--- js/game.js
+++ js/game.js
@@ -1,8 +1,23 @@
 //create new scene
 let gameScene = new Phaser.Scene('Game');
 
+var velocity1X = 0;
+var velocity1Y = 0;
+
+var velocity2X = 0;
+var velocity2Y = 0;
+
 gameScene.init = function(){
+	this.cursors;
+
+	this.velocity1X = 0;
+	this.velocity1Y = 0;
+
+	this.velocity2X = 0;
+	this.velocity2Y = 0;
+
 	this.toobig = false;
+	this.accel = 10;
 };
 
 //load assets
@@ -27,29 +42,49 @@ gameScene.create = function(){
 	console.log(bg);
 	console.log(this);
 
+	//Create controls
+	cursors = this.input.keyboard.createCursorKeys();
+    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
 	//create player sprite (depth 1)
-	let player = this.add.sprite(gameW/2,gameH/2, 'player');
-	player.depth = 1;
-	player.setScale(1,1);
+	//if you don't set the body as active it won't collide with the world bounds
+	this.player1 = this.physics.add.sprite(gameW/2,gameH/2, 'player')
+	this.player1.depth = 1;
+	this.player1.setScale(1,1);
 	//player.scaleX = ...;
 	//player.x = ...;
+	this.player1.setCollideWorldBounds(true);
+
+	this.player2 = this.physics.add.sprite(gameW/2,gameH/2, 'player')
+	this.player2.depth = 1;
+	this.player2.setCollideWorldBounds(true);
 
 	//create enemy sprite (depth 0)
-	this.enemy0 = this.add.sprite(gameW/3, gameH/3, 'enemy');
+	this.enemy0 = this.physics.add.sprite(gameW/3, gameH/3, 'enemy')
+	this.enemy0.setCollideWorldBounds(true);
 
 	//create a second enemy sprite (depth 0)
-	this.enemy1 = this.add.sprite(gameW/1.5, gameH/1.5, 'enemy');
+	this.enemy1 = this.physics.add.sprite(gameW/1.5, gameH/1.5, 'enemy')
 	this.enemy1.displayWidth = 200;
 	this.enemy1.flipX = true;
 	//enemy1.setAngle(-60);
 	//enemy1.angle += 45; //Degrees
 	//enemy1.setRotation(Math.PI / 4);
-	this.enemy1.rotation = Math.PI / 4; //Radians
+	//this.enemy1.rotation = Math.PI / 4; //Radians
+	this.enemy1.setCollideWorldBounds(true);
+
+	this.physics.world.enable([ this.player1, this.player2, this.enemy0, this.enemy1 ]);
 };
 
 //this is called up to 60 times per second
 gameScene.update = function(){
 	this.enemy1.angle += 1;
+	this.enemy1.x += this.accel * Math.cos(this.enemy1.angle/180*Math.PI);
+	this.enemy1.y += this.accel * Math.sin(this.enemy1.angle/180*Math.PI);
+
 	if (this.toobig == false){
 		this.enemy0.displayWidth += 1;
 		this.enemy0.displayHeight += 1;
@@ -63,6 +98,94 @@ gameScene.update = function(){
 	}else if (this.enemy0.displayWidth <= 100){
 		this.toobig = false;
 	}
+
+	velocity1X = Math.min(velocity1X, 300);
+    velocity1X = Math.max(velocity1X, -300);
+    velocity1Y = Math.min(velocity1Y, 300);
+    velocity1Y = Math.max(velocity1Y, -300);
+    this.player1.setVelocityX(velocity1X);
+    this.player1.setVelocityY(velocity1Y);
+
+    velocity2X = Math.min(velocity2X, 300);
+    velocity2X = Math.max(velocity2X, -300);
+    velocity2Y = Math.min(velocity2Y, 300);
+    velocity2Y = Math.max(velocity2Y, -300);
+    this.player2.setVelocityX(velocity2X);
+    this.player2.setVelocityY(velocity2Y);
+
+    if (cursors.left.isDown)
+    {
+        velocity1X += -300;
+        this.player1.setVelocityX(velocity1X);
+    }
+    else if (cursors.right.isDown)
+    {
+        velocity1X += 300;
+        this.player1.setVelocityX(velocity1X);
+    }
+
+    if (cursors.up.isDown)
+    {
+        velocity1Y += -300;
+        this.player1.setVelocityY(velocity1Y);
+    }
+    else if (cursors.down.isDown)
+    {
+        velocity1Y += 300;
+        this.player1.setVelocityY(velocity1Y);
+    }
+    
+        if (velocity1X>0){
+            velocity1X += -7.5;
+        }else if (velocity1X<0){
+            velocity1X += 7.5;
+        }
+
+        if (velocity1Y>0){
+            velocity1Y += -7.5;
+        }else if (velocity1Y<0){
+            velocity1Y += 7.5;
+        }
+
+    if (keyA.isDown)
+    {
+        velocity2X += -300;
+        this.player2.setVelocityX(velocity2X);
+    }
+    else if (keyD.isDown)
+    {
+        velocity2X += 300;
+        this.player2.setVelocityX(velocity2X);
+    }
+
+    if (keyW.isDown)
+    {
+        velocity2Y += -300;
+        this.player2.setVelocityY(velocity2Y);
+    }
+    else if (keyS.isDown)
+    {
+        velocity2Y += 300;
+        this.player2.setVelocityY(velocity2Y);
+    }
+    
+        if (velocity2X>0){
+            velocity2X += -7.5;
+        }else if (velocity2X<0){
+            velocity2X += 7.5;
+        }
+
+        if (velocity2Y>0){
+            velocity2Y += -7.5;
+        }else if (velocity2Y<0){
+            velocity2Y += 7.5;
+        }
+
+    this.physics.world.collide(this.player1, this.player2);
+    this.physics.world.collide(this.player1, this.enemy0);
+    this.physics.world.collide(this.player1, this.enemy1);
+    this.physics.world.collide(this.player2, this.enemy0);
+    this.physics.world.collide(this.player2, this.enemy1);
 };
 
 //set the configuration of the game
@@ -70,7 +193,18 @@ let config = {
 	type: Phaser.AUTO, // Phaser will use WebGL and if not he will use canvas API
 	width: window.innerWidth -15,
 	height: window.innerHeight -15,
-	scene: gameScene
+	parent: 'phaser-example',
+	title: "Gaia's Souls",
+	url: "http://192.168.1.195:8080/GaiasSoul",
+	version: "0.0.3",
+	physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+	scene: gameScene,
+	fps: 60
 };
 console.log(window.innerWidth -15);
 console.log(window.innerHeight -15);
