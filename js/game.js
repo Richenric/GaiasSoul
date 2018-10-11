efd4543edec384f25d50a8c74c8c7179906bdd38
Lukasha707 <43203369+Lukasha707@users.noreply.github.com>
2018-10-11 09:45:43 +0200

    game.js comentado
    Se han añadido comentarios en español explicando el código

--- js/game.js
+++ js/game.js
@@ -1,31 +1,34 @@
 //Mierdas mias, ignoradlas pls
 /*let mainMenuScene = new Phaser.Scene('MainMenu');
 let onGameScene = new Phaser.Scene('OnGame');*/
+//game.js contiene el código que crea el juego, crea la configuración global a todo el juego, coordina que escenas se van a usar y declara las variables globales
 
-//set the configuration of the game
+//Creación de la config del juego
 let config = {
-    type: Phaser.AUTO, // Phaser will use WebGL and if not he will use canvas API
-    width: window.innerWidth -15,
-    height: window.innerHeight -15,
-    parent: 'phaser-example',
-    title: "Gaia's Souls",
-    url: "http://192.168.1.195:8080/GaiasSoul",
-    version: "0.0.3",
-    physics: {
-        default: 'arcade',
+    type: Phaser.AUTO, // Con AUTO, phaser intentará usar WEBGL antes que CANVAS
+    width: window.innerWidth -15, //Configuramos el ancho del canvas para que se ajuste al ancho de pantalla mostrada
+    height: window.innerHeight -15, //Configuramos el alto del canvas para que se ajuste al ancho de pantalla mostrada
+    title: "Gaia's Souls", //Titulo en el menú de phaser al inspeccionar
+    url: "http://192.168.1.195:8080/GaiasSoul", //Url en el menú de phaser al inspeccionar
+    version: "0.0.4", //Version en el menú de phaser al inspeccionar
+    physics: { //Configuramos las físicas que se van a usar en el juego
+        default: 'arcade', //Usamos físicas ARCADE
         arcade: {
-            debug: true
+            debug: true //Usamos el modo DEBUG mientras desarrollamos para ver cajas de colision y vectores de movimiento, luego se pone a false
         }
     },
-    scene: [offGameScene],
+    scene: [offGameScene], //Declaramos que escenas se van a usar y en que orden
     fps: 60
 };
-console.log(window.innerWidth -15);
-console.log(window.innerHeight -15);
 
-//create a new game and past the configuration
+//Testeos
+console.log(window.innerWidth -15); //Testeo del tamaño de ventana
+console.log(window.innerHeight -15); //Testeo del tamaño de ventana
+
+//Creación de un nuevo juego y asignación de config
 let game = new Phaser.Game(config);
 
+//Declaración de variables globales
 var velocity1X = 0;
 var velocity1Y = 0;
 
