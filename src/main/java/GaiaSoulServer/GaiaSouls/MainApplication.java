85060743b2ff686098e59c76a3a3f873da364641
Lukasha707 <43203369+Lukasha707@users.noreply.github.com>
2018-11-21 12:48:43 +0100

    Arreglado peticion de ip
    Se ha eliminado la peticion de Ip por alerta y se ha automatizado

--- src/main/java/GaiaSoulServer/GaiaSouls/MainApplication.java
+++ src/main/java/GaiaSoulServer/GaiaSouls/MainApplication.java
@@ -9,29 +9,7 @@ import java.io.PrintWriter;
 
 @SpringBootApplication
 public class MainApplication {
-
 	public static void main(String[] args) throws FileNotFoundException {
 		SpringApplication.run(MainApplication.class, args);
-		
-		InetAddress ip;
-		String hostaddress;
-		try {
-			ip = InetAddress.getLocalHost();
-			hostaddress = ip.getHostAddress();
-			System.out.println("The ip address: " + ip);
-			System.out.println("hostaddress: " + hostaddress);
-			
-			try(PrintWriter out = new PrintWriter("address.txt")){
-				out.println(hostaddress);
-			}catch(FileNotFoundException w){
-				
-			}
-			
-		
-		}catch(UnknownHostException e){
-			e.printStackTrace();
-		}
-		
-		
 	}
 }
