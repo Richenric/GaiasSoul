7524a9a16f17794a69848f1512369e0452bc75df
Richenric <Richenric@users.noreply.github.com>
2018-11-27 15:18:48 +0100

    Correcciones en el loging menu
    Adecuamos la fuente del texto a la del lobby, redimensionamos el cuadro de escritura de texto e introducimos interactividad a los botones para que se resalten al pasar el ratón por encima

--- src/main/resources/static/js/userName.js
+++ src/main/resources/static/js/userName.js
@@ -2,10 +2,14 @@ let userScene = new Phaser.Scene('enterUser');
 userScene.init = function(){}
 
 userScene.preload = function(){
-    this.load.image('userNW','assets/sprites/enterUsernameWindow.png'); this.load.image('okDis','assets/sprites/OkDisable.png');
+    this.load.image('userNW','assets/sprites/enterUsernameWindow.png'); 
+    this.load.image('introName','assets/sprites/InroUserName.png'); 
+    this.load.image('okDis','assets/sprites/OkDisable.png');
     this.load.image('okOn','assets/sprites/OkOver.png');
     this.load.image('okOff','assets/sprites/OkRelax.png');
     this.load.image('bg','assets/sprites/background3.png');
+    this.load.image('bBack','assets/sprites/backButton.png');
+    this.load.image('bBackOver','assets/sprites/backButtonOVER.png');
 }
 
 userScene.create = function(){
@@ -16,7 +20,39 @@ userScene.create = function(){
     
     //ENTER USERNAME//W
     let userNW = this.add.sprite(gameW/2,gameH/2, 'userNW');
-    
+ 
+    //ENTER TEX HERE//
+    let introName = this.add.sprite(gameW/2-175,gameH/2, 'introName');
+ 
+    //BACK//
+        //BACK OUT//
+    let bBack = this.add.sprite(gameW/2+635,gameH/2+245, 'bBack').setInteractive();
+        bBack.setAlpha(1);
+        bBack.setScale(0.75);
+            //OVER//Back Out
+        bBack.on('pointerover', function (pointer) { 
+            this.setAlpha(0);
+            bBackOver.setAlpha(1);});
+            //OUT//Back Out
+        bBack.on('pointerout', function (pointer) { 
+            this.setAlpha(1);
+            bBackOver.setAlpha(0);});
+        //BACK OVER//
+    let bBackOver = this.add.sprite(gameW/2+635,gameH/2+245, 'bBackOver').setInteractive();
+        bBackOver.setAlpha(0);
+        bBackOver.setScale(0.75);
+            //CLICK//Back Over
+        bBackOver.on('pointerdown', function (pointer) {
+            userScene.scene.switch(mainMenuScene);});
+            //OVER//Back Over
+        bBackOver.on('pointerover', function (pointer, gameObject) {
+            this.setAlpha(1);
+            bBack.setAlpha(0);});
+            //OUT//Back Over
+        bBackOver.on('pointerout', function (pointer, gameObject) {
+            this.setAlpha(0);
+            bBack.setAlpha(1);});
+
     //OK//B
         //DISABLE// --> EVITAR K SE ACCEDA AL LOBBY SIN INTRODUCIR UN NOMBRE
     let okDB = this.add.sprite(gameW/2+200,gameH/2, 'okDis');
@@ -47,8 +83,8 @@ userScene.create = function(){
         okOnB.on('pointerout', function (pointer, gameObject) {
             this.setAlpha(0);
             okOffB.setAlpha(1);});
-    
-    textEntry = this.add.text(gameW/2-325,gameH/2-12, '', { font: '32px Courier', fill: '#ffff00' });
+    //HERE IS THE TEXT//
+    textEntry = this.add.text(gameW/2-365,gameH/2-12, '', { font: '32px verdana', fill: '#555555' });
         this.input.keyboard.on('keydown', function (event) {
             if (event.keyCode === 8 && textEntry.text.length > 0){
                 textEntry.text = textEntry.text.substr(0, textEntry.text.length - 1);
@@ -58,6 +94,8 @@ userScene.create = function(){
             }
                     
         });
+    
+    
 }
 
 userScene.update = function(){}
